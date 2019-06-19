f3688d2919699580477fedceb62276e1610b7426
vitalii <dumavb777@gmail.com>
2019-06-19 20:23:17 +0300

    sol

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const ACTION_UPDATE_ITEM = 'actionUpdateItem';
 const ACTION_EDIT_ITEM = 'actionEditItem';
 const ACTION_CHANGE_INPUT_VALUE = 'actionChangeInputValue';
 const ul = document.getElementById('ul');
-let InitialState = {
+const initialState = {
     clothes: [
         'Apron',
         'Belt',
@@ -14,81 +14,73 @@ let InitialState = {
         'Hat'
     ],
     inputValue: '',
-    editItemText: '',
+    textIndex: null,
 };
 
-const actionEditItem = editItemText => {
+const actionEditItem = payload => {
     return {
         type: ACTION_EDIT_ITEM,
-        editItemText
+        payload
     };
 };
 
-const actionUpdateItem = {
-    type: ACTION_UPDATE_ITEM
+const actionUpdateItem = () => {
+    return {
+        type: ACTION_UPDATE_ITEM
+    };
 };
 
-const actionChangeInputValue = value => {
+const actionChangeInputValue = payload => {
     return {
         type: ACTION_CHANGE_INPUT_VALUE,
-        value,
+        payload,
     }
 };
 
-const store = Redux.createStore(getNextState);
+const store = Redux.createStore(reducer);
 
-function getNextState(state = InitialState, action) {
-    let newState;
-
+function reducer(state = initialState, action) {
     switch (action.type) {
         case ACTION_EDIT_ITEM :
-            newState = {
+            return {
                 ...state,
-                inputValue: action.editItemText,
-                editItemText: action.editItemText
+                inputValue: state.clothes[action.payload],
+                textIndex: action.payload,
             };
-            break;
         case ACTION_CHANGE_INPUT_VALUE :
-            newState = {
+            return {
                 ...state,
-                inputValue: action.value
+                inputValue: action.payload
             };
-            break;
         case ACTION_UPDATE_ITEM :
-            if (state.inputValue.trim() === '') {
-                const index = state.clothes.indexOf(state.editItemText);
-                state.clothes.splice(index, 1)
-            }
-            newState = {
-                clothes: state.clothes.map(clothe => {
-                    if (state.editItemText === clothe) {
-                        return state.inputValue;
-                    }
-                    return clothe;
-                }),
-                inputValue: '',
-                editItemText: ''
-            };
-            break;
-        default :
-            newState = state;
-    }
+            state.clothes[state.textIndex] = state.inputValue;
 
-    return newState;
+            if (state.inputValue.trim() === '') {
+                state.clothes.splice(state.textIndex, 1);
+            }
+            return {
+                clothes: state.clothes,
+                inputValue: '',
+                textIndex: null,
+            };
+        default :
+            return state;
+    }
 }
 
-function showClothes() {
+function render() {
+    const { clothes, textIndex, inputValue} = store.getState();
     const container = ul;
 
-    while(container.firstChild) {
+    while (container.firstChild) {
         ul.removeChild(container.firstChild);
     }
 
-    store.getState().clothes.forEach(clothe => {
+    clothes.forEach((clothe, index) => {
         const li = document.createElement('li');
         li.setAttribute('class', 'item');
 
-        if (store.getState().editItemText !== clothe) {
+        if (textIndex !== index) {
             const span = document.createElement('span');
             const link = document.createElement('a');
 
@@ -96,21 +88,21 @@ function showClothes() {
             link.textContent = 'Edit';
             link.setAttribute('class', 'edit');
 
-            link.addEventListener('click', event =>
-                store.dispatch(actionEditItem(event.target.parentElement.children[0].textContent)));
+            link.addEventListener('click', () =>
+                store.dispatch(actionEditItem(index)));
 
             li.appendChild(span);
             li.append(link);
             ul.append(li)
         }
-        if (store.getState().editItemText === clothe) {
+        if (textIndex === index) {
             const input = document.createElement('input');
-            input.value = store.getState().inputValue;
+            input.value = inputValue;
             input.addEventListener('input', event =>
                 store.dispatch(actionChangeInputValue(event.target.value)));
             input.addEventListener('keydown', event => {
                 if (event.key === "Enter") {
-                    store.dispatch(actionUpdateItem)
+                    store.dispatch(actionUpdateItem())
                 }
             });
 
@@ -125,7 +117,7 @@ function showClothes() {
 }
 
 store.subscribe(() => {
-    showClothes();
+    render();
 });
 
-document.addEventListener('load', showClothes());
+document.addEventListener('load', render());
